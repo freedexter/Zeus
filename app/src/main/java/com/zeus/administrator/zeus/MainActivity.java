3bdd08ff0925f31499e5d91b694659631672fc13
freedexter <wangxg0419@sina.com>
2017-04-20 18:06:39 +0800

    20170420-2

--- app/src/main/java/com/zeus/administrator/zeus/MainActivity.java
+++ app/src/main/java/com/zeus/administrator/zeus/MainActivity.java
@@ -1,13 +1,59 @@
 package com.zeus.administrator.zeus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SlidingMenu menu;
+    private String stat;
+    private TextView editName=null;
+    private String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSlidingMenu();
+        initExtras();
+
+    }
+
+    private void initSlidingMenu() {
+
+        // configure the SlidingMenu
+        menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        // 设置触摸屏幕的模式
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+//        menu.setShadowDrawable(R.drawable.shadow);
+
+        // 设置滑动菜单视图的宽度
+        //menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setBehindWidth(400);
+        // 设置渐入渐出效果的值
+        menu.setFadeDegree(0.35f);
+        /**
+         * SLIDING_WINDOW will include the Title/ActionBar in the content
+         * section of the SlidingMenu, while SLIDING_CONTENT does not.
+         */
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        //为侧滑菜单设置布局
+        menu.setMenu(R.layout.layout_left_menu);
+
+    }
+    private void initExtras(){
+
+        editName = (TextView) findViewById(R.id.username);
+        Intent i = new Intent();
+        i = this.getIntent();
+        Bundle b = i.getExtras();
+        username = b.getString("username");
+        stat = b.getString("stat");
+        //  Toast.makeText(InitActivity.this, "hello! "+username, Toast.LENGTH_SHORT).show();
+        editName.setText(username);
     }
 }
